18f79974e8cc1e673f0f16b7ebc45ff3baf56f63
resghst <hank181017@gmail.com>
2018-03-13 00:13:13 +0800

    add some annotation in down.js. 2018/3/13

--- down.js
+++ down.js
@@ -1,15 +1,17 @@
+//you can download image by this code
+//image-downloader is a js download image package
+
 var download = require('image-downloader')
 
 // Download to a directory and save with the original filename
 const options = {
-    url: 'https://imgur.com/1rxbmvW.jpg',
-    dest: './'                  // Save to /path/to/dest/image.jpg
-  }
-   
-  download.image(options)
-    .then(({ filename, image }) => {
-		  console.log('File saved to '+ filename )
-    }).catch((err) => {
-      throw err
-    })
+  url: 'yout image link', //url link
+  dest: 'your file path'  // Saved image link
+}
+download.image(options)
+.then(({ filename, image }) => {
+  console.log('File saved to '+ filename )
+}).catch((err) => {
+  throw err
+})
    
